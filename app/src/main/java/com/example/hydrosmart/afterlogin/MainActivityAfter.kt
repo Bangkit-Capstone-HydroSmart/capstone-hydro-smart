90c3ccf67214f174e05bab2824745c4c38630a22
Maulana Husein <maulanahusein03@gmail.com>
2023-11-30 09:08:21 +0700

    add starter project

---
+++ app/src/main/java/com/example/hydrosmart/afterlogin/MainActivityAfter.kt
@@ -0,0 +1,36 @@
+package com.example.hydrosmart.afterlogin
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.hydrosmart.R
+import com.example.hydrosmart.databinding.ActivityMainAfterBinding
+
+class MainActivityAfter : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainAfterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainAfterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main_after)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
